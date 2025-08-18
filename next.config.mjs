ca2bef3bc796a20ddc7346622ab0cab1554d220c
ae-314 <aaron.elcheson@mail.mcgill.ca>
2025-08-18 17:55:26 -0400

    updated main for test + config edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  output: 'export',              // build static files into /out
+  images: { unoptimized: true }, // no server-side image optimizer
+};
 export default nextConfig;
